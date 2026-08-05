ff3e64d0c2c5d0f9a11e72ea0fb840546fbe78a2
Manish Biswas <manish20was@gmail.com>
2026-08-06 04:08:23 +0530

    Fix formatting in staging SQL for reviews data extraction

--- food_delivery/models/staging/stg_reviews.sql
+++ food_delivery/models/staging/stg_reviews.sql
@@ -6,7 +6,7 @@ r.restaurant_id::string as restaurant_id,
 r.rating::number as rating,
 r.comment::string as comment,
 r.review_date::DATE as review_date,
-res.city as city,
+res.city as city
 from {{ source('raw', 'reviews') }} r
 left join {{ ref('stg_restaurants') }} res on r.restaurant_id = res.restaurant_id
 where r.comment is not null
